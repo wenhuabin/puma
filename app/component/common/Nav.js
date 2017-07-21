829c2f6950e9d76de38ad1bff281519ec52a9ccf
wenhuabin <huabinwen@gmail.com>
2017-07-22 01:11:06 +0800

    添加Header Nav组件

--- app/component/common/Nav.js
+++ app/component/common/Nav.js
@@ -1,27 +1,64 @@
 require('scss/common/nav.scss');
 import {Component} from 'react';
+import { Link } from 'react-router';
 
 class HeaderNav extends Component {
-  constructor(props){
-	super(props);
-  }
-	
-  render() {
-    return (
-        <header className="header">
-  			<img className="logo" src={require('images/logo.jpg')} />
-            <div className="brand">SpaceX</div>
-            <div className="logout">退出登录</div>
-            <div className="setting-box">
-                <div className="setting">设置</div>
-                <ul className="options">
-                    <li className="option">个人信息</li>
-                    <li className="option">修改密码</li>
-                </ul>
-			</div>
-        </header>
-	);
-  }
+    constructor(props, context) { super(props, context);
+        this.state = {
+            isCurrent: -1,
+            isChildShow: [],
+			isListShow: -1,
+            navs: [
+                {name: '主页', key: 0, url: '/'},
+                {name: 'Demo', key: 1, url: '', child: [{name: '图形', key: 10, url: '/', child: [{name: 'Canvas', key: 110, url: '/canvas'}, {name: 'SVG', key: 111, url: '/'}]},{name: 'Echart组件', key: 11, url: '/echart'},{name: 'D3-组件', key: 12, url: '/d3'},{name: 'ChartJS', key: 13, url: '/cjs'}, {name: '通用组件', key: 14, url: '/component'}]},
+                {name: 'About', key: 3, url: '/about'},
+            ],
+        };
+    }
+
+    setCurrent(key){
+		this.setState({isCurrent: key});
+    }
+
+    toggleShowList(key){
+
+	}
+
+    toggleShowChild(key){
+        var tmp = this.isChildShow;
+        tmp.includes(key) ? tmp.splice(tmp.indexOf(key), 1) : tmp.push(key)
+		this.setState({isChildShow: tmp});
+    }
+      
+    render() {
+        return (
+            <header className="header">
+          		<img className="logo" src={require('images/logo.jpg')} />
+                <div className="brand">SpaceX</div>
+                <div className="logout">退出登录</div>
+              	<div className="nav-list">
+              	    <Link className="nav" to={'/about'}>关于我</Link>
+        	  	  </div>
+              	  <div className="nav-list" style={{}}>
+              	      <div className="nav" onMouseOver={()=>this.toggleShowList(2)}>设置</div>
+              	      <ul className="items">
+              	          <li className="item"><Link to={'/cjs'}>ChartJS</Link></li>
+              	          <li className="item"><Link to={'/canvas'}>Canvas</Link></li>
+              	      </ul>
+        	  	  </div>
+              	  <div className="nav-list" >
+              	      <div className="nav" onMouseOver={()=>this.toggleShowList(2)}>组件</div>
+              	      <ul className="items">
+              	          <li className="item"><Link to={'/cjs'}>ChartJS</Link></li>
+              	          <li className="item"><Link to={'/canvas'}>Canvas</Link></li>
+              	      </ul>
+        	  	  </div>
+              	  <div className="nav-list">
+                  	  <Link className="nav" to={'/about'}>首页</Link>
+        		  </div>
+            </header>
+        );
+    }
 };
 
 module.exports = HeaderNav;
