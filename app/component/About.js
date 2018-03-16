68c545ac57bf757d888f07b6b413b17d9e4f693b
huabinwen <huabinwen@gmail.com>
2018-03-16 22:44:31 +0800

    update job journey

--- app/component/About.js
+++ app/component/About.js
@@ -23,7 +23,7 @@ class About extends Component {
 				<li>基于&nbsp;REM&nbsp;移动端适配</li>
 				<li>数据可视化 / 地图集成</li>
 				<li>前端组件化、模块化和工程化开发</li>
-				<li>Webpack、Parcel&nbsp;配置和优化; &nbsp;前端性能优化 </li>
+				<li>Webpack、Parcel&nbsp;配置和优化;&nbsp;&nbsp;前端性能优化 </li>
 				<li>Nginx / HTTPS（HTTP2）/ BBR </li>
 				<li>熟悉 PWA 相关技术</li>
 				<li>了解小程序开发</li>
@@ -31,7 +31,7 @@ class About extends Component {
             </ol>
 			<h2>项目经历</h2>
 			<ol>
-				<li>个人：
+				<li>个人项目：
                     <a href="https://github.com/wenhuabin/teddy" target="_blank" title="基于&nbsp;Express/Mysql/Redis/jQuery/Webpack&nbsp;技术栈，通过&nbsp;rem&nbsp;和屏幕&nbsp;dpr&nbsp;适配移动版页面">Teddy&nbsp;项目（Express）</a>
 				    <a href="https://github.com/wenhuabin/merlin" target="_blank" title="基于&nbsp;Vue/Vuex/vue-router/Webpack/SASS&nbsp;技术栈，前后端分离">Merlin&nbsp;项目（Vue）</a>
 				    <a href="https://github.com/wenhuabin/puma" target="_blank" title="基于&nbsp;React/react-redux/react-router/SASS/Webpack&nbsp;技术栈">Puma&nbsp;项目（React）</a>
@@ -39,7 +39,7 @@ class About extends Component {
                     <a href="#">微信小程序</a>
                     <a href="#">PWA 项目</a>
                 </li>
-				<li className="hide">蔚来汽车：<a href="https://login.nio.com/login" target="_blank" title="基于&nbsp;Java Spring/JSP/jQuery/SASS&nbsp;技术栈，通过&nbsp;rem&nbsp;和屏幕&nbsp;dpr&nbsp;适配移动版页面">单点登录系统 </a><span title="基于&nbsp;Express/EJS&nbsp;模板&nbsp;/jQuery&nbsp;技术栈，通过&nbsp;rem&nbsp;和屏幕&nbsp;dpr&nbsp;适配移动版页面">LifeStyle项目</span><span title="基于&nbsp;PHP/Vue/Vuex/vue-router/Webpack/SASS&nbsp;技术栈，前后端分离">车辆后台管理系统</span></li>
+				<li className="no-hide">NIO：<a href="https://login.nio.com/login" target="_blank" title="基于&nbsp;Java Spring/JSP/jQuery/SASS&nbsp;技术栈，通过&nbsp;rem&nbsp;和屏幕&nbsp;dpr&nbsp;适配移动版页面">单点登录系统 </a><span title="基于&nbsp;PHP/Vue/Vuex/vue-router/Webpack/SASS&nbsp;技术栈，前后端分离">XX&nbsp;后台管理系统</span></li>
 				<li>小熊快跑：<a href="https://guanjia.xxkuaipao.com" target="_blank" title="基于&nbsp;React/react-redux/react-router/SASS/Webpack技术栈，前后端分离">小熊管家</a><span title="基于&nbsp;Flask/Jinjia2/LESS&nbsp;技术栈">包月健身管理后台/商家后台</span></li>
 			</ol>
 		    <footer className="footer">{'©'}  Copyright 2018 by @wenhuabin. All Right Reserved.</footer>
